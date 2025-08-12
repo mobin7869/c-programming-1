9c9fe2bf8592b0cd43cd9789e127d63f07894551
mobin7869 <mobingandhi2023@gmail.com>
2025-08-12 17:12:49 +0530

    Add files via upload

---
+++ Mobin1.c
@@ -0,0 +1,17 @@
+//ONLINE c compiler to run c program
+#include <stdio.h>
+int main()
+{
+printf("Enter number\n");
+int number;
+scanf("%d",&number);
+if(number%2==0)
+{
+printf("Number is even");
+}
+else
+{
+printf("Number is odd");
+}
+return 0;
+}
