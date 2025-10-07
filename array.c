b33c2ed3a38a57c3af14e961621e8b9c6647df74
mobin7869 <mobingandhi2023@gmail.com>
2025-10-07 17:35:51 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i;
+int a[10];
+printf("enter value of arrays :");
+int *arraypoint;
+arraypoint = &a[i];
+for(i=0;i<10;i++)
+{
+scanf("\n%d",arraypoint);}
+return 0;
+}
